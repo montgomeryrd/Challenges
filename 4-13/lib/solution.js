689e8cdd6e666803ebb2986402e9c13c3fecef03
montgomeryrd <montgomery_rd@yahoo.com>
2018-05-27 10:21:01 -0700

    4-13 challenge

---
+++ 4-13/lib/solution.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function champernowneDigit(n) {
+  let champernowne = [];
+  let count = 0;
+  
+  if(n === 0 || typeof n !== 'number') return 'NaN'
+  
+  while(count !== n) {
+    champernowne.push(count);
+    count++;
+  }
+  
+  champernowne = champernowne.join('').split('');
+  return parseInt(champernowne[n - 1]);
+};
+
