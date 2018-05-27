265323063c88989bb79d0f06a242a78c629b553d
montgomeryrd <montgomery_rd@yahoo.com>
2018-05-27 10:30:41 -0700

    5-27 challenge

--- 5-27/__test__/solution.test.js
+++ 5-27/__test__/solution.test.js
@@ -9,19 +9,19 @@ describe('Solution', () => {
         
         let arr1 = [12, 10, 8, 12, 7, 6, 4, 10, 12];
 
-        it('should return Champernowne\'s Digit: 0', () => {
+        it('should return 12 as the highest ranking number', () => {
             expect(Solution(arr1)).toEqual(12);
         });
 
         let arr2 = [12, 10, 8, 12, 7, 6, 4, 10, 12, 10];
 
-        it('should return Champernowne\'s Digit: 4', () => {
+        it('should return 12 as the highest ranking number', () => {
             expect(Solution(arr2)).toEqual(12);
         });
 
         let arr3 = [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10];
 
-        it('should return Champernowne\'s Digit: 5', () => {
+        it('should return 3 as the highest ranking number', () => {
             expect(Solution(arr3)).toEqual(3);
         });
     });
