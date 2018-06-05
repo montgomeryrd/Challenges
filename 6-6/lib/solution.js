c2504ec3a5c3b2817879f628347c1f49b2b552c2
montgomeryrd <montgomery_rd@yahoo.com>
2018-06-05 02:25:59 -0700

    6-6 challenge

--- 6-6/lib/solution.js
+++ 6-6/lib/solution.js
@@ -27,7 +27,7 @@ module.exports = function solution(number){
 // function solution(number){
 //     var sum = 0;
     
-//     for(var i = 1;i< number; i++){
+//     for(var i = 1 ; i < number; i++){
 //         if(i % 3 == 0 || i % 5 == 0){
 //             sum += i
 //         }
