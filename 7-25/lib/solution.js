c9e710bea14ff68fc63704a4f641ec4e02d79670
montgomeryrd <montgomery_rd@yahoo.com>
2018-09-14 12:48:36 -0700

    7-25 challenge

--- 7-25/lib/solution.js
+++ 7-25/lib/solution.js
@@ -25,3 +25,25 @@ module.exports = function  closest(strng) {
   
   return [[smallest[0], locations[0], Number(origin[0])], [smallest[1], locations[1], Number(origin[1])]];
 }
+
+//Better code:
+
+// function  closest(strng) {
+//   let min = Number.MAX_SAFE_INTEGER, idx = -1;
+  
+//   if (strng.length === 0) return [];
+
+//   const weight = (num) =>
+//     [...num + ''].reduce((a, b) => a + +b, 0);
+
+//   const arr = strng.split(' ')
+//     .map((x, i) => [weight(x), i, +x])
+//     .sort((a, b, x = a[0] - b[0]) => !x ? a[1] - b[1] : x);
+
+//   for (let j = 1; arr[j]; j++) {
+//     const diff = arr[j][0] - arr[j - 1][0];
+//     diff < min && (min = diff, idx = j);
+//   }
+
+//   return [arr[idx - 1], arr[idx]];
+// };
